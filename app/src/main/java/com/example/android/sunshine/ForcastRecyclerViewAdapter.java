eb58448c577dcf4de45a6e683eb23e4caf9148fc
DESKTOP-UV38N4H\Abdel Rahman <abdelrahman.ahmed@elibre.io>
2018-11-08 10:24:45 +0200

    finishing converting item List from textView summary only to each item to be view

--- app/src/main/java/com/example/android/sunshine/ForcastRecyclerViewAdapter.java
+++ app/src/main/java/com/example/android/sunshine/ForcastRecyclerViewAdapter.java
@@ -6,7 +6,9 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+
 import com.example.android.sunshine.data.SunshineContract.*;
 
 import com.example.android.sunshine.data.SunshineContract;
@@ -31,6 +33,7 @@ public class ForcastRecyclerViewAdapter extends RecyclerView.Adapter<ForcastRecy
         View view = LayoutInflater.from(this.context).inflate(R.layout.recycler_view_item, parent, false);
         return new ForcastViewHolder(view);
     }
+
     public void SwapCursor(Cursor cursor) {
         if (cursor != null) {
             if (dataSource != null)
@@ -39,6 +42,7 @@ public class ForcastRecyclerViewAdapter extends RecyclerView.Adapter<ForcastRecy
             notifyDataSetChanged();
         }
     }
+
     public void SetDataSource(Cursor dataSource) {
         this.dataSource = dataSource;
         notifyDataSetChanged();
@@ -51,40 +55,55 @@ public class ForcastRecyclerViewAdapter extends RecyclerView.Adapter<ForcastRecy
 
     @Override
     public int getItemCount() {
-        if(dataSource==null)
+        if (dataSource == null)
             return 0;
         return dataSource.getCount();
     }
 
     public class ForcastViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
-        TextView textView;
+        TextView textViewDate;
+        TextView textViewDescription;
+        TextView textViewHighTemperature;
+        TextView textViewLowTemperature;
+        ImageView iconWeather;
+
 
         public ForcastViewHolder(View itemView) {
             super(itemView);
-            textView = (TextView) itemView.findViewById(R.id.id_item_textView);
+            textViewDate = (TextView) itemView.findViewById(R.id.textViewDate);
+            textViewDescription = (TextView) itemView.findViewById(R.id.textViewDecription);
+            textViewHighTemperature = (TextView) itemView.findViewById(R.id.textViewHighTemperature);
+            textViewLowTemperature = (TextView) itemView.findViewById(R.id.textViewLowTemperature);
+            iconWeather = (ImageView) itemView.findViewById(R.id.imageViewWeatherImage);
         }
 
         public void OnBind(int position) {
-          if( dataSource.moveToPosition(position))
-          {
-              int columnDateIndex= dataSource.getColumnIndex(WeatherEntry.COLUMN_DATE);
-              int columnWeatherIDIndex= dataSource.getColumnIndex(WeatherEntry.COLUMN_WEATHER_ID);
-              int columnWeatherMaxTempIndex= dataSource.getColumnIndex(WeatherEntry.COLUMN_MAX_TEMP);
-              int columnWeatherMinTempIndex= dataSource.getColumnIndex(WeatherEntry.COLUMN_MIN_TEMP);
-              long dateMillis= dataSource.getLong(columnDateIndex);
-              String dateString= SunshineDateUtils.getFriendlyDateString(context, dateMillis,true);
-              int weatherID= dataSource.getInt(columnWeatherIDIndex);
-              String description = SunshineWeatherUtils.getStringForWeatherCondition(context, weatherID);
-              double maxTemp=dataSource.getDouble(columnWeatherMaxTempIndex);
-              double minTemp=dataSource.getDouble(columnWeatherMaxTempIndex);
-              String highAndLowTemperature = SunshineWeatherUtils.formatHighLows(context, maxTemp, minTemp);
-              String weatherSummary=dateString+" - "+description+" - "+highAndLowTemperature;
-              textView.setText(weatherSummary);
-              this.itemView.setTag(dateMillis);
-          }
+            if (dataSource.moveToPosition(position)) {
+                int columnDateIndex = dataSource.getColumnIndex(WeatherEntry.COLUMN_DATE);
+                int columnWeatherIDIndex = dataSource.getColumnIndex(WeatherEntry.COLUMN_WEATHER_ID);
+                int columnWeatherMaxTempIndex = dataSource.getColumnIndex(WeatherEntry.COLUMN_MAX_TEMP);
+                int columnWeatherMinTempIndex = dataSource.getColumnIndex(WeatherEntry.COLUMN_MIN_TEMP);
+                long dateMillis = dataSource.getLong(columnDateIndex);
+                String dateString = SunshineDateUtils.getFriendlyDateString(context, dateMillis, true);
+                int weatherID = dataSource.getInt(columnWeatherIDIndex);
+                String description = SunshineWeatherUtils.getStringForWeatherCondition(context, weatherID);
+                double maxTemp = dataSource.getDouble(columnWeatherMaxTempIndex);
+                double minTemp = dataSource.getDouble(columnWeatherMinTempIndex);
+                //  String highAndLowTemperature = SunshineWeatherUtils.formatHighLows(context, maxTemp, minTemp);
+                String highTemperature = SunshineWeatherUtils.formatTemperature(context, maxTemp);
+                String minTemperature = SunshineWeatherUtils.formatTemperature(context, minTemp);
+                //   String weatherSummary=dateString+" - "+description+" - "+highAndLowTemperature;
+                textViewDate.setText(dateString);
+                textViewDescription.setText(description);
+                textViewHighTemperature.setText(highTemperature);
+                textViewHighTemperature.setText(minTemperature);
+                int weatherImageId = SunshineWeatherUtils
+                        .getArtResourceForWeatherCondition(weatherID);
+                iconWeather.setImageResource(weatherImageId);
+                this.itemView.setTag(dateMillis);
+            }
             itemView.setOnClickListener(this);
         }
-
 
 
         @Override
